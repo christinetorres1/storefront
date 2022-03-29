474fb7ba43c3bbd69cdc073194fff6ecadaae80e
Christine Torres <christinejoytorres1@outlook.com>
2022-03-29 14:31:27 -0700

    set up done

--- components/PageTitle/PageTitle.js
+++ components/PageTitle/PageTitle.js
@@ -4,7 +4,7 @@ import styles from "./styles.module.scss";
 
 function PageTitle({ children, tagline, title, ...props }) {
   return (
-    <header className="styles.pageTitle">
+    <header className={styles.pageTitle}>
       <h1>{title}</h1>
       <p>{tagline}</p>
     </header>
